b54d0357ba7ef34aac721ab2f3ca87c158ff8be9
dangkim <waitmail01@yahoo.com>
2019-06-15 00:48:30 +0800

    ccc

--- webpack.config.js
+++ webpack.config.js
@@ -43,16 +43,17 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|jpg|gif|svg)$/,
+                test: /\.(gif|png|jpe?g|svg)$/i,
                 use: [
-                  {
-                    loader: 'file-loader',
-                    // options: {
-                    //   name: '[name].[hash:8].[ext]',
-                    //   outputPath: 'assets/'
-                    // }
-                  }
-                ]
+                    'file-loader',
+                    {
+                    loader: 'image-webpack-loader',
+                    options: {
+                        //bypassOnDebug: true, // webpack@1.x
+                        //disable: true, // webpack@2.x and newer
+                    },
+                    },
+                ],
             }
         ]
     },
