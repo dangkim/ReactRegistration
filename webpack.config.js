9d6238d73b1345585d66112dfde659102da78c50
dangkim <waitmail01@yahoo.com>
2019-06-15 00:29:39 +0800

    bbb

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: './src/index.jsx',
     mode: 'production',
@@ -17,9 +18,24 @@ module.exports = {
                 loader: 'babel-loader'
             },
             {
-                test:/\.css$/,
-                use:['style-loader','css-loader']
-            },
+                test: /\.css$/,
+                use: [
+                  {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                      // you can specify a publicPath here
+                      // by default it uses publicPath in webpackOptions.output
+                      //publicPath: '../',
+                      //hmr: process.env.NODE_ENV === 'development',
+                    },
+                  },
+                  'css-loader',
+                ],
+              },
+            // {
+            //     test:/\.css$/,
+            //     use:['style-loader','css-loader']
+            // },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/, 
                 use: [
@@ -41,6 +57,12 @@ module.exports = {
         ]
     },
     plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+          }),
         new HtmlWebpackPlugin({template: './src/index.html'})
     ],
     devServer: {
