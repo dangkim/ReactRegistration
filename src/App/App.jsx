d59e39c6042277162914d241af82e6946b4da1ae
dangkim <waitmail01@yahoo.com>
2019-06-15 00:09:35 +0800

    aaa

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -8,6 +8,7 @@ import { PrivateRoute } from '../_components';
 import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
 import { RegisterPage } from '../RegisterPage';
+import css from './app.css';
 
 class App extends React.Component {
     constructor(props) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+const path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-    mode: 'development',
+    entry: './src/index.jsx',
+    mode: 'production',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js'
+    },
     resolve: {
         extensions: ['.js', '.jsx']
     },
@@ -10,12 +15,34 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 loader: 'babel-loader'
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/, 
+                use: [
+                'file-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    // options: {
+                    //   name: '[name].[hash:8].[ext]',
+                    //   outputPath: 'assets/'
+                    // }
+                  }
+                ]
             }
         ]
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/index.html'})
+    ],
     devServer: {
         historyApiFallback: true
     },
