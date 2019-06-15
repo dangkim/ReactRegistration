5f24bb222b8590d6ca7a5e35f1ef86517fdd32d3
dangkim <waitmail01@yahoo.com>
2019-06-15 17:28:01 +0800

    eee

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -3,18 +3,18 @@ import { combineReducers } from 'redux';
 import { authentication } from './authentication.reducer';
 import { registration } from './registration.reducer';
 import { users } from './users.reducer';
-import { brand } from './brand.reducer';
+import { brands } from './brands.reducer';
 import { influencers } from './influencers.reducer';
-import { campaign } from './campaign.reducer';
+import { campaigns } from './campaigns.reducer';
 import { alert } from './alert.reducer';
 
 const rootReducer = combineReducers({
   authentication,
   registration,
   users,
-  brand,
+  brands,
   influencers,
-  campaign,
+  campaigns,
   alert
 });
 
