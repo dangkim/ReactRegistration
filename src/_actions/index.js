22cb14488973c42461ff53eb54b472ec38132f5e
dangkim <waitmail01@yahoo.com>
2019-06-20 09:27:07 +0800

    publish

--- src/_actions/index.js
+++
@@ -1,5 +0,0 @@
-export * from './alert.actions';
-export * from './user.actions';
-export * from './campaign.actions';
-export * from './brands.actions';
-export * from './influencer.actions';
--- src/_services/index.js
+++
@@ -1,4 +0,0 @@
-export * from './user.service';
-export * from './brand.service';
-export * from './campaign.service';
-export * from './influencer.service';
